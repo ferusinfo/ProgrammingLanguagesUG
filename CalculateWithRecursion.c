78ee4f9c3c952a190102ebe11c9692f741855fb5
Maciej Kołek <kontakt@ferus.info>
2012-05-28 08:50:23 +0200

    Added 6th program

---
+++ CalculateWithRecursion.c
@@ -0,0 +1,48 @@
+/*
+Ten program liczy rekurencyjnie NWD ze wzoru Euklidesa
+Dział Rekurencje - zadanie 4
+@autor Maciej Kolek 5E
+2012 Uniwersytet Gdanski
+*/
+
+//Includes
+#include <stdio.h>
+
+//Defines
+#define FALSE 0
+#define TRUE 1
+
+//Get Data
+void getNumbers(int *a, int *b)
+{
+	printf("Podaj liczbe a:  ");
+	scanf("%i", a);
+	printf("Podaj liczbe b:  ");
+	scanf("%i", b);
+}
+
+int CalculateWithRecursion(int a, int b)
+{
+	while (a*b != 0)
+	{
+		if (a >= b)
+		{
+			return CalculateWithRecursion(a%b,b);
+		}
+		else
+		{
+			return CalculateWithRecursion(a,b%a);
+		}
+	}
+	return a+b;
+}
+
+int main()
+{
+	int a, b;
+
+	getNumbers(&a, &b);
+	printf("NWD: %i \n\n", CalculateWithRecursion(a,b));
+
+	return 0;
+}
